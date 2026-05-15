5ed273df6be47feaf38352a1844625f6eb15626f
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 23:18:09 +0200

    home

--- src/component/home.js
+++ src/component/home.js
@@ -15,7 +15,7 @@ function HomePage(props) {
     }, []);
     return (
         <>
-            <p style={{ textAlign: "center", color: "white" }}>Bienvenue</p>
+            <p style={{ textAlign: "center", color: "white" }}>Bienvenue,</p>
             <p style={{ textAlign: "center", color: "white" }}>Ici, tu peux ouvrir un booster Pokémon toutes les heures, et utiliser tes points de chaîne Twitch pour en ouvrir encore plus ou capturer des Pokémon. <br />
                 Complète ton Pokédex, remplis ta collection de cartes, construis ton profil pour flex sur les lives !<br />
                 Tout ça, c’est du taff, alors si tu veux me soutenir, passe sur mes streams et pense à lacher ton follow, tu me régalerais !</p>
