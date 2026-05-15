fe0336b5590645e6059bd7b6370af6a29258183a
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 01:15:02 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
                       <Route path="/" element={<HomePage />} />
                   </Routes>
               </BrowserRouter>
-              <NavBar cookies={cookies} />
       </header>
     </div>
   );
