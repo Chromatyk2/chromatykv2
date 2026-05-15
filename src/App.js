87c89d6131d45a5f4c7127dac76a109091f27359
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 22:18:29 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
-
+import 'react-tooltip/dist/react-tooltip.css'
+import HomePage from './component/home.js';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+            </Routes>
+         </BrowserRouter>
+    );
 }
 
 export default App;
