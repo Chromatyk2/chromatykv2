4e6236c608b339d3198337c4f25b2294927e6439
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 22:24:52 +0200

    home

--- src/component/home.js
+++ src/component/home.js
@@ -1,7 +1,6 @@
 import React,{useState, useEffect} from 'react';
 import Axios from 'axios'
 import {Link} from "react-router-dom";
-import moment from "moment";
 
 function HomePage(props) {
   return (
