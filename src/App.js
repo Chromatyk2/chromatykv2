3e00c6edbb1073afc019c4679bb8f55171cd8bf2
pierre schuvey <chromatykretro@gmail.com>
2026-05-15 22:32:46 +0200

    home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import logo from './logo.svg';
 import './App.css';
-import 'react-tooltip/dist/react-tooltip.css'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import HomePage from './component/home.js';
+
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-         </BrowserRouter>
-    );
+  return (
+    <div className="App">
+          <header className="App-header">
+              <BrowserRouter>
+                  <Routes>
+                      <Route path="/" element={<HomePage />} />
+                  </Routes>
+              </BrowserRouter>
+      </header>
+    </div>
+  );
 }
 
 export default App;
