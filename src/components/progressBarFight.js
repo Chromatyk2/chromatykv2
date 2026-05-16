43fdf339abb625db61d3bd1181efb240dd4761a5
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 07:33:06 +0200

    fix

--- src/components/progressBarFight.js
+++ src/components/progressBarFight.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom'
-import ReactPaginate from 'react-paginate';
-import Axios from 'axios'
-import Pagination from './paginate.js';
 import '../App.css'
-import moment from 'moment';
-import { Tooltip } from "react-tooltip";
-import PokedexTeam from "./pokedexTeam";
-import Modal from "react-modal";
-import $ from "jquery";
 
 function ProgressBarFight() {
     return (            
-        <progress className={"progressBarFight"} max="100" value="70">70%</progress>
+        <div className={"progressBarFightExternal"}>
+            <div className={"progressBarFightInternal"}></div>
+        </div>
     )
 }
 
