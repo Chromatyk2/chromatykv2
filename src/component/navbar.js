137ffb4d0f9cc82a7c05d333817f0d3331eaf06e
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 06:50:12 +0200

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -60,11 +60,12 @@ function NavBar(props) {
     //    }
     //}, [props.cookies]);
     function deployNav() {
-        //const isActive = document.getElementById("navBar").classList.contains('expended');
-        //document.getElementById("navBar").remove('expended');
-        //if (!isActive) {
-        //    document.getElementById("navBar").classList.add('active');
-        //}
+        const isActive = document.getElementById("navBar").classList.contains('expended');
+        if (!isActive) {
+            document.getElementById("navBar").classList.add('expended');
+        } else {
+            document.getElementById("navBar").remove('expended');
+        }
     }
     return (
         <>
