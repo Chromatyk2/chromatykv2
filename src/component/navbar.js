ff1e4ea3d9044cd39177a0886290ed4df3baeec6
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 04:24:55 +0200

    fix

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -62,7 +62,7 @@ function NavBar(props) {
     return (
         <>
             <div className="halfCircle">
-                <svg onclick="openMenu()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#83d7d7" stroke-linecap="round" stroke-linejoin="round" height="75" width="75">
+                <svg onclick="openMenu()" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#0E1E56" stroke-linecap="round" stroke-linejoin="round" height="55" width="55">
                     <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0 -18 0" stroke-width="2"></path>
                     <path d="M9 12a3 3 0 1 0 6 0 3 3 0 1 0 -6 0" stroke-width="2"></path>
                     <path d="M3 12h6" stroke-width="2"></path>
