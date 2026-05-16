72e89012305226e9e3ccd820104280638fb2b47e
pierre schuvey <chromatykretro@gmail.com>
2026-05-16 03:39:08 +0200

    fix

--- src/services/log.js
+++ src/services/log.js
@@ -3,6 +3,7 @@ import { useCookies } from 'react-cookie';
 import Axios from 'axios'
 import env from "react-dotenv";
 import { Navigate } from "react-router-dom";
+import Login from 'auth.services.js';
 
 
 
@@ -12,5 +13,8 @@ function Log() {
             window.location.href = "https://chromatykv2.netlify.app/";
         }, 2000);
     }, []);
+    return (
+        <Login />
+    )
 }
 export default Log;
