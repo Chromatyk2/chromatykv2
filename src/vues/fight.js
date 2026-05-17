f789dfe005d8718f62a0e798ac4b8a77e583e6b8
pierre schuvey <chromatykretro@gmail.com>
2026-05-17 03:42:07 +0200

    sprites

--- src/vues/fight.js
+++ src/vues/fight.js
@@ -22,7 +22,7 @@ function Fight() {
     }
 
     function getRandomPokemon() {
-        Axios.post('/api/getRandomPokemon',
+        Axios.get('/api/getRandomPokemon',
             {
                 tier: Math.floor((Math.random() * 4) + 1)
             }
