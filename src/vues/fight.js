bd6c6353f3699202320cef60cce8013cf7b4db9b
pierre schuvey <chromatykretro@gmail.com>
2026-05-17 23:41:44 +0200

    getRandomPokemon

--- src/vues/fight.js
+++ src/vues/fight.js
@@ -145,7 +145,7 @@ function Fight() {
         <div className={"fightContainer"}>
             {pokemon &&
                 <>
-                <div style={{ top: "85px" }} onClick={fleeFight} className={"fightActionsFlee"}>
+                <div style={{ top: "10px" }} onClick={fleeFight} className={"fightActionsFlee"}>
                         < img src={"/disc.png"} />
                         <p>Save</p>
                         </div>
