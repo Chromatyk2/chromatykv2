0b58081b5245f682102e2c39974d60c16b7bbde9
pierre schuvey <chromatykretro@gmail.com>
2026-05-17 09:34:02 +0200

    getRandomPokemon

--- src/vues/fight.js
+++ src/vues/fight.js
@@ -138,21 +138,48 @@ function Fight() {
             {pokemon ?
                 <>
                     <div className={"fightActionsContainer"}>
-                        <div onClick={addLoveS} className={"fightActions"}>
-                            < img src={"/exps.png"} />
-                            <p>Bonbon S</p>
-                            <p>x 1</p>
-                        </div>
-                        <div onClick={addLoveM} className={"fightActions"}>
-                            < img src={"/expm.png"} />
-                            <p>Bonbon M</p>
-                            <p>x 1</p>
-                        </div>
-                        <div onClick={addLoveL} className={"fightActions"}>
-                            < img src={"/expl.png"} />
-                            <p>Bonbon L</p>
-                            <p>x 1</p>
-                        </div>
+                        {currentLove < maxLove ?
+                            <>
+                                <div onClick={addLoveS} className={"fightActions"}>
+                                    < img src={"/exps.png"} />
+                                    <p>Bonbon S</p>
+                                    <p>x 1</p>
+                                </div>
+                                <div onClick={addLoveM} className={"fightActions"}>
+                                    < img src={"/expm.png"} />
+                                    <p>Bonbon M</p>
+                                    <p>x 1</p>
+                                </div>
+                                <div onClick={addLoveL} className={"fightActions"}>
+                                    < img src={"/expl.png"} />
+                                    <p>Bonbon L</p>
+                                    <p>x 1</p>
+                                </div>
+                            </>
+                            :
+                            <>
+                                <div onClick={addLoveS} className={"fightActions"}>
+                                    < img src={"/ball.png"} />
+                                    <p>Pokéball</p>
+                                    <p>x 1</p>
+                                </div>
+                                <div onClick={addLoveM} className={"fightActions"}>
+                                    < img src={"/great.png"} />
+                                    <p>Super Ball</p>
+                                    <p>x 1</p>
+                                </div>
+                                <div onClick={addLoveL} className={"fightActions"}>
+                                    < img src={"/ultra.png"} />
+                                    <p>Hyper Ball</p>
+                                    <p>x 1</p>
+                                </div>
+                                <div onClick={addLoveL} className={"fightActions"}>
+                                    < img src={"/master.png"} />
+                                    <p>Master Ball</p>
+                                    <p>x 1</p>
+                                </div>
+                            </>
+                        }
                     </div>
                 </>
                 :
