5f7512841e5a82fde7c2e72d42de2d94bc6199c6
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 01:27:12 +0200

    fix

--- src/vues/safari.js
+++ src/vues/safari.js
@@ -22,7 +22,7 @@ function Fight() {
         Axios
             .get("/api/getSafari/" + cookies.user.data[0].id)
             .then(function (response) {
-                if (response.length > 0) {
+                if (response.data.length == 0) {
                     setSafari(true);
                 }
                 setCurrentLove(response.data[0].love)
