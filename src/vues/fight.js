ff577d521dc6bcc6d74c9694bc73925518e8943b
pierre schuvey <chromatykretro@gmail.com>
2026-05-17 20:31:52 +0200

    getRandomPokemon

--- src/vues/fight.js
+++ src/vues/fight.js
@@ -169,22 +169,22 @@ function Fight() {
                             </>
                             :
                             <>
-                                <div onClick={() => catchPokemon(0, pokemon.tier)} className={"fightActions"}>
+                                <div onClick={() => catchPokemon(1, pokemon.tier)} className={"fightActions"}>
                                     < img src={"/ball.png"} />
                                     <p>Pokéball</p>
                                     <p>x 1</p>
                                 </div>
-                                <div onClick={() => catchPokemon(1, pokemon.tier)} className={"fightActions"}>
+                                <div onClick={() => catchPokemon(2, pokemon.tier)} className={"fightActions"}>
                                     < img src={"/great.png"} />
                                     <p>Super Ball</p>
                                     <p>x 1</p>
                                 </div>
-                                <div onClick={() => catchPokemon(2,pokemon.tier)} className={"fightActions"}>
+                                <div onClick={() => catchPokemon(3,pokemon.tier)} className={"fightActions"}>
                                     < img src={"/ultra.png"} />
                                     <p>Hyper Ball</p>
                                     <p>x 1</p>
                                 </div>
-                                <div onClick={() => catchPokemon(3,pokemon.tier)} className={"fightActions"}>
+                                <div onClick={() => catchPokemon(4,pokemon.tier)} className={"fightActions"}>
                                     < img src={"/master.png"} />
                                     <p>Master Ball</p>
                                     <p>x 1</p>
