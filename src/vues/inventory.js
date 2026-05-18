93f959673bace1eb6e7792337c96dd6876b1a69e
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 19:18:34 +0200

    fix

--- src/vues/inventory.js
+++ src/vues/inventory.js
@@ -118,7 +118,7 @@ function Inventory(props) {
                 :
                 <>
                     <p className="pseudoProfil">Votre inventire est vide, récupère des objets et des boosters sur les stream de Chromatyk</p>
-                    <a href="https://twitch.tv/chromatyk" target="blank_">Twitch</a>
+                    <a className={"twitchLink"} href="https://twitch.tv/chromatyk" target="blank_">Twitch</a>
                 </>
             }
          </>
