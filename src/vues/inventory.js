aecb0f5189efcb19038c44dadb1ae07f62bf1d2a
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 04:57:16 +0200

    fix

--- src/vues/inventory.js
+++ src/vues/inventory.js
@@ -5,6 +5,7 @@ import { useCookies } from 'react-cookie';
 
 function Inventory(props) {
     const [cookies, setCookie] = useCookies();
+    const [inventory, setInventory] = useCookies(null);
     useEffect(() => {
         Axios.post('/api/createInventory', {
             user: cookies.user.data[0].id
@@ -13,7 +14,7 @@ function Inventory(props) {
             Axios
             .get("/api/getInventory/" + cookies.user.data[0].id)
             .then(function (response) {
-                console.log(response);
+                setInventory(response.data[0])
             })
         })
     }, []);
@@ -24,53 +25,57 @@ function Inventory(props) {
                 <div className={"honeyActions"}>
                     <img style={{ filter: "drop-shadow(white 0px 0px 5px) hue-rotate(352deg) contrast(1.1)" }} src={"/honey.png"} />
                     <p>Miel<br />Classique</p>
+                    <p>x {inventory.honey}</p>
                 </div>
                 <div className={"honeyActions"}>
                     <img style={{ filter: "drop-shadow(red 0px 0px 5px) hue-rotate(303deg) contrast(1.1)" }} src={"/honey.png"} />
                     <p>Miel<br />Légendaire</p>
+                    <p>x {inventory.legendary}</p>
                 </div>
                 <div className={"honeyActions"}>
                     <img style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3)" }} src={"/honey.png"} />
                     <p>Miel<br />Chromatique</p>
+                    <p>x {inventory.shiny}</p>
                 </div>
                 <div class="honeyActions">
                     <img src="/honey.png" style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3) invert(1)" }} />
                     <p>Miel<br />Négatif</p>
+                    <p>x {inventory.negative}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/exps.png"} />
                     <p>Bonbon S</p>
-                    <p>x 1</p>
+                    <p>x {inventory.samall}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/expm.png"} />
                     <p>Bonbon M</p>
-                    <p>x 1</p>
+                    <p>x {inventory.medium}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/expl.png"} />
                     <p>Bonbon L</p>
-                    <p>x 1</p>
+                    <p>x {inventory.large}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/ball.png"} />
                     <p>Pokéball</p>
-                    <p>x 1</p>
+                    <p>x {inventory.pokeball}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/great.png"} />
                     <p>Super Ball</p>
-                    <p>x 1</p>
+                    <p>x {inventory.greatball}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/ultra.png"} />
                     <p>Hyper Ball</p>
-                    <p>x 1</p>
+                    <p>x {inventory.ultraball}</p>
                 </div>
                 <div className={"honeyActions"}>
                     < img src={"/master.png"} />
                     <p>Master Ball</p>
-                    <p>x 1</p>
+                    <p>x {inventory.msterball}</p>
                 </div>
             </div>
         </>
