119869daacd0f2602b52eaa1ce9ca1f6fa18fa07
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 05:22:08 +0200

    fix

--- src/vues/inventory.js
+++ src/vues/inventory.js
@@ -21,64 +21,68 @@ function Inventory(props) {
     }, []);
     return (
         <>
-            <p className="pseudoProfil">Inventaire</p>
-            <div className={"inventoryContainer"}>
-                <div className={"honeyActions"}>
-                    <img style={{ filter: "drop-shadow(white 0px 0px 5px) hue-rotate(352deg) contrast(1.1)" }} src={"/honey.png"} />
-                    <p>Miel<br />Classique</p>
-                    <p>x {inventory.honey}</p>
+            {inventory &&
+                <>
+                <p className="pseudoProfil">Inventaire</p>
+                <div className={"inventoryContainer"}>
+                    <div className={"honeyActions"}>
+                        <img style={{ filter: "drop-shadow(white 0px 0px 5px) hue-rotate(352deg) contrast(1.1)" }} src={"/honey.png"} />
+                        <p>Miel<br />Classique</p>
+                        <p>x {inventory.honey}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        <img style={{ filter: "drop-shadow(red 0px 0px 5px) hue-rotate(303deg) contrast(1.1)" }} src={"/honey.png"} />
+                        <p>Miel<br />Légendaire</p>
+                        <p>x {inventory.legendary}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        <img style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3)" }} src={"/honey.png"} />
+                        <p>Miel<br />Chromatique</p>
+                        <p>x {inventory.shiny}</p>
+                    </div>
+                    <div class="honeyActions">
+                        <img src="/honey.png" style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3) invert(1)" }} />
+                        <p>Miel<br />Négatif</p>
+                        <p>x {inventory.negative}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/exps.png"} />
+                        <p>Bonbon S</p>
+                        <p>x {inventory.samall}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/expm.png"} />
+                        <p>Bonbon M</p>
+                        <p>x {inventory.medium}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/expl.png"} />
+                        <p>Bonbon L</p>
+                        <p>x {inventory.large}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/ball.png"} />
+                        <p>Pokéball</p>
+                        <p>x {inventory.pokeball}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/great.png"} />
+                        <p>Super Ball</p>
+                        <p>x {inventory.greatball}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/ultra.png"} />
+                        <p>Hyper Ball</p>
+                        <p>x {inventory.ultraball}</p>
+                    </div>
+                    <div className={"honeyActions"}>
+                        < img src={"/master.png"} />
+                        <p>Master Ball</p>
+                        <p>x {inventory.msterball}</p>
+                    </div>
                 </div>
-                <div className={"honeyActions"}>
-                    <img style={{ filter: "drop-shadow(red 0px 0px 5px) hue-rotate(303deg) contrast(1.1)" }} src={"/honey.png"} />
-                    <p>Miel<br />Légendaire</p>
-                    <p>x {inventory.legendary}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    <img style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3)" }} src={"/honey.png"} />
-                    <p>Miel<br />Chromatique</p>
-                    <p>x {inventory.shiny}</p>
-                </div>
-                <div class="honeyActions">
-                    <img src="/honey.png" style={{ filter: "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3) invert(1)" }} />
-                    <p>Miel<br />Négatif</p>
-                    <p>x {inventory.negative}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/exps.png"} />
-                    <p>Bonbon S</p>
-                    <p>x {inventory.samall}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/expm.png"} />
-                    <p>Bonbon M</p>
-                    <p>x {inventory.medium}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/expl.png"} />
-                    <p>Bonbon L</p>
-                    <p>x {inventory.large}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/ball.png"} />
-                    <p>Pokéball</p>
-                    <p>x {inventory.pokeball}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/great.png"} />
-                    <p>Super Ball</p>
-                    <p>x {inventory.greatball}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/ultra.png"} />
-                    <p>Hyper Ball</p>
-                    <p>x {inventory.ultraball}</p>
-                </div>
-                <div className={"honeyActions"}>
-                    < img src={"/master.png"} />
-                    <p>Master Ball</p>
-                    <p>x {inventory.msterball}</p>
-                </div>
-            </div>
+                </>
+            }            
         </>
     )
 }
