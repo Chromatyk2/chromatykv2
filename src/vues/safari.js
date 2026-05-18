d4d6dd9d5a4f0e38579bffe81fb53f1e363f0361
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 20:23:12 +0200

    fix

--- src/vues/safari.js
+++ src/vues/safari.js
@@ -217,9 +217,8 @@ function Fight() {
                 <>
                     <div className={"honeyActionsContainer"}>
                         {inventory &&
-                            inventory.map((val, key) => {
+                        inventory.filter(item => item.slug === "honey" || item.slug === "legendary" || item.slug === "shiny" || item.slug === "negative").map((val, key) => {
                                 return (
-                                    (val.slug === "honey" || val.slug === "legendary" || val.slug === "shiny" || val.slug === "negative") &&
                                     val.quantity > 0 && (
                                         <div onClick={() => consomeHoney(val.slug)} className={"honeyActions"}>
                                             <img style={{ filter: val.slug == "honey" ? "drop-shadow(white 0px 0px 5px) hue-rotate(352deg) contrast(1.1)" : val.slug == "shiny" ? "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3)" : val.slug == "legendary" ? "drop-shadow(red 0px 0px 5px) hue-rotate(303deg) contrast(1.1)" : val.slug == "negative" && "drop-shadow(gold 0px 0px 5px) hue-rotate(15deg) contrast(1.3) invert(1)" }} src={"/" + val.slug == "honey" || val.slug == "shiny" || val.slug == "legendary" || val.slug == "negative" ? "honey.png" : val.slug + ".png"} />
