5eb98f9787a308a903fc032ce21f4aa8ce3f041e
pierre schuvey <chromatykretro@gmail.com>
2026-05-18 05:14:38 +0200

    fix

--- src/vues/inventory.js
+++ src/vues/inventory.js
@@ -14,8 +14,8 @@ function Inventory(props) {
             Axios
             .get("/api/getInventory/" + cookies.user.data[0].id)
             .then(function (response) {
-                setInventory(response.data);
-                console.log(response.data);
+                setInventory(response.data[0]);
+                console.log(response.data[0]);
             })
         })
     }, []);
