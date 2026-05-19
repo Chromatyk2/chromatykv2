a89ca7b00198f33ba08d08447cc26c74f6d8c7b0
pierre schuvey <chromatykretro@gmail.com>
2026-05-19 03:56:03 +0200

    fix

--- src/vues/safari.js
+++ src/vues/safari.js
@@ -311,7 +311,8 @@ function Fight() {
             }
             {pokemon &&
                     <div className={"fightActionsContainer"}>
-                        {currentLove < maxLove ?
+                    {onCatch === false ?
+                        currentLove < maxLove ?
                         <>
                             {inventory.filter(item => (item.slug === "exps" && item.quantity > 0) || (item.slug === "expm" && item.quantity > 0) || (item.slug === "expl" && item.quantity > 0)).length < 1 &&
                                 <div style={{background:"none"}} className={"emptyInventory"}>
