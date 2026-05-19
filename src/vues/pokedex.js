9a3070653bd04d505382ee07400bfac205f30747
pierre schuvey <chromatykretro@gmail.com>
2026-05-19 08:21:03 +0200

    fix

--- src/vues/pokedex.js
+++ src/vues/pokedex.js
@@ -8,8 +8,6 @@ function Pokedex() {
     const [cookies, setCookie] = useCookies();
     const [pokedex, setPokedex] = useState(null);
     const [filteredPokedex, setFilteredPokedex] = useState(null);
-    const [isShiny, setIsShiny] = useState("normal");
-    const [isNegative, setIsNegative] = useState(0);
     const [gen, setGen] = useState(1);
     const [genList, setGenList] = useState([1,2,3,4,5,6,7,8,9])
     useEffect(() => {
@@ -31,6 +29,11 @@ function Pokedex() {
     return (
         <div className={"globalContainer"}>
             <div className={"dexContainer"}>
+                <div className={"genFilter"}>
+                    <button onClick={() => filterGen(0)} value={0}>Tous</button>
+                    <button onClick={() => filterGen(1)} value={1}>Gen {1}</button>
+                    <button onClick={() => filterGen(2)} value={2}>Gen {2}</button>
+                </div>
                 <div className={"genFilter"}>
                     <button className={gen === 0 && "active"} onClick={() => filterGen(0)} value={0}>Toutes</button>
                     {genList.map((val, key) => {
