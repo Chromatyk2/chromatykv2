654084a2e5537303267ad23f2fe1d93ec4ec73c6
pierre schuvey <chromatykretro@gmail.com>
2026-05-19 08:56:15 +0200

    fix

--- src/vues/pokedex.js
+++ src/vues/pokedex.js
@@ -45,9 +45,9 @@ function Pokedex() {
         <div className={"globalContainer"}>
             <div className={"dexContainer"}>
                 <div className={"genFilter"}>
-                    <button onClick={() => filterForm(0)}>Tous</button>
-                    <button onClick={() => filterForm("Shiny")}>Shiny</button>
-                    <button onClick={() => filterForm(1)}>Négtif</button>
+                    <button className={isShiny === 0 && isNegative === 0 && "active"} onClick={() => filterForm(0)}>Tous</button>
+                    <button className={isShiny === 1 && "active"} onClick={() => filterForm("Shiny")}>Shiny</button>
+                    <button className={isNegative === 1 && "active"} onClick={() => filterForm(1)}>Négtif</button>
                 </div>
                 <div className={"genFilter"}>
                     <button className={gen === 0 && "active"} onClick={() => filterGen(0)} value={0}>Toutes</button>
