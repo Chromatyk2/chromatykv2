bc201875de3043be1cb74f3079646897a656e384
pierre schuvey <chromatykretro@gmail.com>
2026-05-19 05:21:20 +0200

    fix

--- src/vues/pokedex.js
+++ src/vues/pokedex.js
@@ -7,6 +7,7 @@ import { useCookies } from 'react-cookie';
 function Pokedex() {
     const [cookies, setCookie] = useCookies();
     const [pokedex, setPokedex] = useState(null);
+    const [isShiny, setIsShiny] = useState("normal");
     useEffect(() => {
         Axios
             .get("/api/getPokedex/" + + cookies.user.data[0].id)
@@ -16,7 +17,17 @@ function Pokedex() {
     }, []);
     return (
         <>
-
+            {pokedex &&
+                pokedex.map((val, key) => {
+                    return (
+                        <div className={"dexCard"}>
+                            <span className={"dexName"}>{val.number}</span>
+                            <img className={"dexSprite"} src={"/Sprites/" + isShiny + "/" + val.number + ".gif"} />
+                            <p className={"dexName"}>{val.name}</p>
+                            <p className={"dexDate"}>{moment(val.date).utc().format('DD/MM/YYYY')}</p>
+                        </div >
+                )
+            }
         </>
     );
 }
