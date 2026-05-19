01d1e2efbc66d4dd966c0b69b7db20d82c663c85
pierre schuvey <chromatykretro@gmail.com>
2026-05-19 03:50:37 +0200

    fix

--- src/vues/safari.js
+++ src/vues/safari.js
@@ -29,18 +29,20 @@ function Fight() {
                         Axios
                             .get("/api/getSafari/" + cookies.user.data[0].id)
                             .then(function (response) {
-                                setCurrentLove(response.data[0].love)
-                                setShiny(response.data[0].shiny)
-                                setNegative(response.data[0].negative)
-                                setPokemon({ name: response.data[0].name, number: response.data[0].pokemon, tier: response.data[0].tier })
-                                if (response.data[0].tier == 1) {
-                                    setMaxLove(50);
-                                } else if (response.data[0].tier == 2) {
-                                    setMaxLove(100);
-                                } else if (response.data[0].tier == 3) {
-                                    setMaxLove(150);
-                                } else {
-                                    setMaxLove(250);
+                                if (response.data.length > 0) {
+                                    setCurrentLove(response.data[0].love)
+                                    setShiny(response.data[0].shiny)
+                                    setNegative(response.data[0].negative)
+                                    setPokemon({ name: response.data[0].name, number: response.data[0].pokemon, tier: response.data[0].tier })
+                                    if (response.data[0].tier == 1) {
+                                        setMaxLove(50);
+                                    } else if (response.data[0].tier == 2) {
+                                        setMaxLove(100);
+                                    } else if (response.data[0].tier == 3) {
+                                        setMaxLove(150);
+                                    } else {
+                                        setMaxLove(250);
+                                    }
                                 }
                             })
                     })
