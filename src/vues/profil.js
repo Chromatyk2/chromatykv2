e13f399ff44f1c172ba51f4357dd2d784fee7441
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 13:19:29 +0200

    flee fight

--- src/vues/profil.js
+++ src/vues/profil.js
@@ -154,7 +154,7 @@ function Profil() {
                                 <p style={{textAlign: "end"}}>{compagnon[0].pokemon}</p>
                                 <p style={{ textAlign: "end" }} className={"levelProfil"}>Niveau {compagnon[0].level}</p>
                             </div>
-                            <div style={{ filter: compagnon[0].negative === 1 "invert(1) ? invert(0)", backgroundColor: colorShiny, backgroundImage: `url("/Sprites/${compagnon[0].shiny === 1 ? "Shiny" : "Normal"}/${compagnon[0].number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center"}} className={"compagnonPicture"}>
+                            <div style={{ filter: compagnon[0].negative === 1 ?"invert(1)" : "invert(0)", backgroundColor: colorShiny, backgroundImage: `url("/Sprites/${compagnon[0].shiny === 1 ? "Shiny" : "Normal"}/${compagnon[0].number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center"}} className={"compagnonPicture"}>
                             </div>
                             {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                         </div>
