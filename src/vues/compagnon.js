17be72605d826af3800f8c223a794a97633de9e1
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 21:33:53 +0200

    flee fight

--- src/vues/compagnon.js
+++ src/vues/compagnon.js
@@ -81,7 +81,7 @@ function Compagnon() {
                 }
                 {chooseCompagnon &&
                     <>
-                        <div className={"dexContainer"}>
+                    <div style={{position:"absolute", top: "-100px",justifyContent:"center"}} className={"dexContainer"}>
                             <div className={"genFilter"}>
                                 <button className={isShiny === 0 && isNegative === 0 && "active"} onClick={() => filterForm(0)}>Normal</button>
                                 <button className={isShiny === 1 && "active"} onClick={() => filterForm("Shiny")}>Shiny</button>
