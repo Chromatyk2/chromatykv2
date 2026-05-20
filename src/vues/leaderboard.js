f262ab6cccb4b0501223b3800bab98cc359b7688
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 15:21:49 +0200

    flee fight

--- src/vues/leaderboard.js
+++ src/vues/leaderboard.js
@@ -16,30 +16,26 @@ function Leaderboard() {
     return (
         <div className={"globalContainer"}>
             <div className={"leaderboardContainer"}>
-            <p style={{ margin: 0 }} >Leaderboard</p>
+            <p style={{ margin: 0 }} >Communauté</p>
             {leaderboard &&
                 leaderboard.map((val, key) => {
                     return (
                         <div class={"leaderboardHeaderContainer"}>
                             <div className={"profilHeader"}>
-                                <div style={{ backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
+                                <div style={{width: "40px",height: "40px", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
                                 </div>
                                 {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                                 <div className={"profilInfos"}>
-                                    <p>{val.login}</p>
-                                    <p className={"levelProfil"}>Niveau {val.level}</p>
+                                    <p style={{fontSize: "14px"}}>{val.login}</p>
+                                    <p style={{ fontSize: "12px" }} className={"levelProfil"}>Niveau {val.level}</p>
                                 </div>
-                            </div>
-                            <div className={"buttonLeaderboard"}>
-                                <button>Profil</button>
-                                <button>Pokedex</button>
                             </div>
                             <div className={"profilHeader"}>
                                 <div className={"profilInfos"}>
-                                    <p style={{ textAlign: "end" }}>{val.pokemon}</p>
-                                    <p style={{ textAlign: "end" }} className={"levelProfil"}>{val.shiny === 1 ? "Shiny" : val.negative === 1 ? "Négatif" : "Classique"}</p>
+                                    <p style={{ fontSize: "14px", textAlign: "end" }}>{val.pokemon}</p>
+                                    <p style={{ fontSize: "12px", textAlign: "end" }} className={"levelProfil"}>{val.shiny === 1 ? "Shiny" : val.negative === 1 ? "Négatif" : "Classique"}</p>
                                 </div>
-                                <div style={{ filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
+                                <div style={{ width: "40px", height: "40px", filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
                                 </div>
                                 {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                             </div>
