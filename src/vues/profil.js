bbea6bf9947f1b4773097b61cc8ef0e4d7c11542
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 09:47:28 +0200

    flee fight

--- src/vues/profil.js
+++ src/vues/profil.js
@@ -136,16 +136,7 @@ function Profil() {
                     <div className={"filterProfil"}>
                         <button className={body === 1 && "active"} onClick={() => changePage(1)}>Profil</button>
                         <button className={body === 2 && "active"} onClick={() => changePage(2)}>Skins</button>
-                    </div>
-
-                    {body === 2 &&
-                        skins &&
-                        skins.length < profil[0].level &&
-                        <div onClick={() => addSkin()} className={"containerOpenSkin"} style={{ position: "absolute" }}>
-                            <p className={"openSkinText"} style={{ position: "relative", top: "50px", left: "0px", background: "rgb(239, 211, 151)", borderRadius: "100%", width: "27px", height: "27px", border: "1px solid #8a9cdd", color: "black" }}>{profil[0].level - skins.length}</p>
-                            <img id={"skinPicture"} loading={"lazy"} style={{ background: "#efd397" }} className={"skinPicture"} src={"/case.png"} />
-                        </div>
-                    }
+                    </div>                    
                     <div className={"profilBody"}>
                         {body === 1 &&
                             <>
@@ -192,6 +183,11 @@ function Profil() {
                                     </>
                                 }
                             </>
+                        }
+                        {body === 2 &&
+                            skins &&
+                            skins.length < profil[0].level &&
+                            <img id={"skinPicture"} loading={"lazy"} style={{ background: "#efd397" }} className={"skinPicture"} src={"/case.png"} />
                         }
                         {body === 2 &&
                             <div className={"skinContainer"}>
