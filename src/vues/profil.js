7ccd2482e868eb3453a30c40f05cdcf798606ccf
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 02:24:15 +0200

    flee fight

--- src/vues/profil.js
+++ src/vues/profil.js
@@ -41,20 +41,6 @@ function Profil() {
                     <p>EXP</p>
                     <p>0/100</p>
                 </div>
-                <div className={"genFilter"}>
-                    <button className={isShiny === 0 && isNegative === 0 && "active"} onClick={() => filterForm(0)}>Normal</button>
-                    <button className={isShiny === 1 && "active"} onClick={() => filterForm("Shiny")}>Shiny</button>
-                    <button className={isNegative === 1 && "active"} onClick={() => filterForm(1)}>Négatif</button>
-                </div>
-                <div className={"genFilter"}>
-                    <button className={gen === 0 && "active"} onClick={() => filterGen(0)} value={0}>Toutes</button>
-                    {genList.map((val, key) => {
-                        return (
-                            <button className={gen === val && "active"} onClick={() => filterGen(val)} value={val}>Gen {val}</button>
-                        )
-                    })}
-
-                </div>
                 {filteredPokedex &&
                     filteredPokedex.filter(item => (item.shiny === isShiny && item.negative === isNegative)).map((val, key) => {
                         return (
