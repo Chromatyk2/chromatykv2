46ac90503bc5ea254219307b1f0b35f711a272db
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 23:13:13 +0200

    flee fight

--- src/vues/compagnon.js
+++ src/vues/compagnon.js
@@ -164,7 +164,11 @@ function Compagnon() {
                     compagnon &&
                     compagnon.length > 0 &&
                     !chooseCompagnon && (
-                        <>
+                    <>
+                        <div style={{ top: "85px" }} onClick={chooseCompgnon} className={"fightActionsFlee"}>
+                            < img src={"/boot.png"} />
+                            <p>Partir</p>
+                        </div>
                             <p className="fightName">{compagnon[0].pokemon}</p>
                             <div className="tierFight">
                                 Nv.{compagnon[0].level}
