66bfac0c5c6fc133229a30279af18c4433472b1f
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 14:35:50 +0200

    flee fight

---
+++ src/vues/leaderboard.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
+import moment from "moment/moment";
+import { useCookies } from 'react-cookie';
+
+
+function Leaderboard() {
+    const [cookies, setCookie] = useCookies
+    const [leaderboard, setLeaderboard] = useCookies();
+    useEffect(() => {
+        Axios.get("/api/getLeaderBoard/")
+            .then((response) => {
+                setLeaderboard(response.data)
+            })
+    }, []);
+    return (
+        <div className={"globalContainer"}>
+            {leaderboard &&
+                leaderboard.map((val, key) => {
+                    return (
+
+                        <div class={"profilHeaderContainer"}>
+                            <div className={"profilHeader"}>
+                                <div style={{ backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
+                                </div>
+                                {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
+                                <div className={"profilInfos"}>
+                                    <p>{cookies.user.data[0].login}</p>
+                                    <p className={"levelProfil"}>Niveau {val.level}</p>
+                                </div>
+                            </div>
+                            <div className={"profilHeader"}>
+                                <div className={"profilInfos"}>
+                                    <p style={{ textAlign: "end" }}>{val.pokemon}</p>
+                                    <p style={{ textAlign: "end" }} className={"levelProfil"}>{val.shiny === 1 ? "Shiny" : val.negative === 1 ? "Négatif" : "Classique"}</p>
+                                </div>
+                                <div style={{ filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
+                                </div>
+                                {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    );
+}
+
+export default Leaderboard;
