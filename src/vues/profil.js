95005f648e5163e0d1bfb928677f48bb00714754
pierre schuvey <chromatykretro@gmail.com>
2026-05-20 06:13:50 +0200

    flee fight

--- src/vues/profil.js
+++ src/vues/profil.js
@@ -10,7 +10,7 @@ import { getColorSync } from 'colorthief';
 
 function Profil() {
     const [cookies, setCookie] = useCookies();
-    const [pokedex, setPokedex] = useState(null);
+    const [profil, setProfil] = useState(null);
     const [maxPokedex, setMaxPokedex] = useState(1198);
     const [images, setImages] = useState(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]);
     const [imagesShiny, setImagesShiny] = useState(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]);
@@ -19,12 +19,12 @@ function Profil() {
     const [color, setColor] = useState(1);
     useEffect(() => {
         Axios
-            .get("/api/getPokedex/" + cookies.user.data[0].id)
+            .get("/api/getProfil/" + cookies.user.data[0].id)
             .then(function (response) {
-                setPokedex(response.data);
+                setProfil(response.data);
                 setIndex();
                 const img = new Image();
-                img.src = "/Badge/Trainer2090.png";
+                img.src = "/Badge/Trainer"+response.data[0].skin+".png";
 
                 img.onload = () => {
                     setColor(getColorSync(img).hex());
@@ -37,75 +37,77 @@ function Profil() {
     }
     return (
         <div className={"globalContainer"}>
-            <div className={"profilContainer"}>
-                <div className={"profilHeader"}>
-                    <img classNme={"profilPicture"} style={{background:color}} src={"/Badge/Trainer2090.png"} />
-                    <div className={"profilInfos"}>
-                        <p>{cookies.user.data[0].login}</p>
-                        <p className={"levelProfil"}>Niveau 1</p>
+            {profil &&
+                <div className={"profilContainer"}>
+                    <div className={"profilHeader"}>
+                        <img classNme={"profilPicture"} style={{ background: color }} src={"/Badge/Trainer"+profil[0].skin+".png"} />
+                        <div className={"profilInfos"}>
+                            <p>{cookies.user.data[0].login}</p>
+                            <p className={"levelProfil"}>Niveau 1</p>
+                        </div>
                     </div>
-                </div>
-                <div className={"textProgressProfil"}>
-                    <p>EXP</p>
-                    <p>100000/1000000</p>
-                </div>
-                <div className={"progressBarProfilExternal"}>
-                    <div style={{ width: "50%" }} className={"progressBarProfilInternal"}>
+                    <div className={"textProgressProfil"}>
+                        <p>EXP</p>
+                        <p>100000/1000000</p>
                     </div>
-                </div>
-                <div className={"filterProfil"}>
-                    <button className={body === 1 && "active"} onClick={() => changePage(1)}>Profil</button>
-                    <button className={body === 2 && "active"} onClick={() => changePage(2)}>Skins</button>
-                </div>
-                <div className={"profilBody"}>
-                    {body === 1 &&
-                        <>
-                            <p style={{ width: "100%", textAlign: "left", fontSize: "15px" }}>Progression</p>
-                            {pokedex &&
-                                <>
-                                    {pokedex.length > 0 &&
-                                        <div className={"boxProfilLarge"}>
-                                            <div className={"profilHeader"}>
-                                                <div className={"profilDex"}>
-                                                    <p>Pokédex Classique</p>
-                                                    <p className={"levelProfil"}>{pokedex.length} / {maxPokedex}</p>
-                                                </div>
-                                                <img src={"/Badge/lv" + images[Math.min(images.length - 1, Math.floor((pokedex.length / maxPokedex) * images.length))] + ".png"} />
-                                            </div>
-                                        </div>
-                                    }
-                                    {pokedex.filter(item => (item.shiny === 1)).length > 0 &&
-                                        <div className={"boxProfilLarge"}>
-                                            <div className={"profilHeader"}>
-                                                <div className={"profilDex"}>
-                                                    <p>Pokédex Shiny</p>
-                                                    <p className={"levelProfil"}>{pokedex.filter(item => (item.shiny === 1)).length} / {maxPokedex}</p>
-                                                </div>
-                                                <img src={"/Badge/lv" + imagesShiny[Math.min(imagesShiny.length - 1, Math.floor((pokedex.filter(item => (item.shiny === 1)).length / maxPokedex) * imagesShiny.length))] + "s.png"} />
-                                            </div>
-                                        </div>
-                                    }
-                                    {pokedex.filter(item => (item.negative === 1)).length > 0 &&
-                                        <>
+                    <div className={"progressBarProfilExternal"}>
+                        <div style={{ width: "50%" }} className={"progressBarProfilInternal"}>
+                        </div>
+                    </div>
+                    <div className={"filterProfil"}>
+                        <button className={body === 1 && "active"} onClick={() => changePage(1)}>Profil</button>
+                        <button className={body === 2 && "active"} onClick={() => changePage(2)}>Skins</button>
+                    </div>
+                    <div className={"profilBody"}>
+                        {body === 1 &&
+                            <>
+                                <p style={{ width: "100%", textAlign: "left", fontSize: "15px" }}>Progression</p>
+                                {profil &&
+                                    <>
+                                        {profil.length > 0 &&
                                             <div className={"boxProfilLarge"}>
                                                 <div className={"profilHeader"}>
                                                     <div className={"profilDex"}>
-                                                        <p>Pokédex Négatif</p>
-                                                        <p className={"levelProfil"}>{pokedex.filter(item => (item.negative === 1)).length}  / {maxPokedex}</p>
+                                                        <p>Pokédex Classique</p>
+                                                        <p className={"levelProfil"}>{profil.length} / {maxPokedex}</p>
                                                     </div>
-                                                    <img src={"/Badge/lv" + imagesShiny[Math.min(imagesShiny.length - 1, Math.floor((pokedex.filter(item => (item.negative === 1)).length / maxPokedex) * imagesShiny.length))] + "n.png"} />
+                                                    <img src={"/Badge/lv" + images[Math.min(images.length - 1, Math.floor((profil.length / maxPokedex) * images.length))] + ".png"} />
                                                 </div>
                                             </div>
+                                        }
+                                        {profil.filter(item => (item.shiny === 1)).length > 0 &&
                                             <div className={"boxProfilLarge"}>
+                                                <div className={"profilHeader"}>
+                                                    <div className={"profilDex"}>
+                                                        <p>Pokédex Shiny</p>
+                                                        <p className={"levelProfil"}>{profil.filter(item => (item.shiny === 1)).length} / {maxPokedex}</p>
+                                                    </div>
+                                                    <img src={"/Badge/lv" + imagesShiny[Math.min(imagesShiny.length - 1, Math.floor((profil.filter(item => (item.shiny === 1)).length / maxPokedex) * imagesShiny.length))] + "s.png"} />
+                                                </div>
                                             </div>
-                                        </>
-                                    }
-                                </>
-                            }
-                        </>
-                    }
+                                        }
+                                        {profil.filter(item => (item.negative === 1)).length > 0 &&
+                                            <>
+                                                <div className={"boxProfilLarge"}>
+                                                    <div className={"profilHeader"}>
+                                                        <div className={"profilDex"}>
+                                                            <p>Pokédex Négatif</p>
+                                                            <p className={"levelProfil"}>{profil.filter(item => (item.negative === 1)).length}  / {maxPokedex}</p>
+                                                        </div>
+                                                        <img src={"/Badge/lv" + imagesShiny[Math.min(imagesShiny.length - 1, Math.floor((profil.filter(item => (item.negative === 1)).length / maxPokedex) * imagesShiny.length))] + "n.png"} />
+                                                    </div>
+                                                </div>
+                                                <div className={"boxProfilLarge"}>
+                                                </div>
+                                            </>
+                                        }
+                                    </>
+                                }
+                            </>
+                        }
+                    </div>
                 </div>
-            </div>
+            }
         </div>
     );
 }
