37a270959ed554ce848f6eea49bd05f14068af09
pierre schuvey <chromatykretro@gmail.com>
2026-05-21 07:31:41 +0200

    flee fight

--- src/vues/home.js
+++ src/vues/home.js
@@ -25,10 +25,23 @@ function HomePage(props) {
                 skin: 9999,
                 compagnon: 0
             }).then(function (response) {
-                Axios.post('/api/addItem', {
+                Axios.post('/api/addCandy', {
                     user: cookies.user.data[0].id,
-                    item: "Lootbox",
-                    slug: "box"
+                    item: "Miel Ordinaire",
+                    slug: "honey",
+                    quantity: 1
+                })
+                Axios.post('/api/addCandy', {
+                    user: cookies.user.data[0].id,
+                    item: "Bonbon S",
+                    slug: "exps",
+                    quantity: 10
+                })
+                Axios.post('/api/addCandy', {
+                    user: cookies.user.data[0].id,
+                    item: "Poke Ball",
+                    slug: "ball",
+                    quantity: 10
                 }).then(function (response) {
                     Axios
                         .get("/api/getShinydex")
