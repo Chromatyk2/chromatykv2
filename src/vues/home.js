a2d9b58e9867bcc87be4df438eb8a58f3b390814
pierre schuvey <chromatykretro@gmail.com>
2026-05-21 07:53:47 +0200

    flee fight

--- src/vues/home.js
+++ src/vues/home.js
@@ -21,7 +21,6 @@ function HomePage(props) {
                 .get("/api/getUser/" + cookies.user.data[0].id)
                 .then(function (response) {
                     if (response.data.length < 1) {
-
                         Axios.post('/api/addProfil', {
                             user: cookies.user.data[0].id,
                             login: cookies.user.data[0].login,
@@ -29,44 +28,49 @@ function HomePage(props) {
                             xp: 0,
                             skin: 9999,
                             compagnon: 0
-                        }).then(function (response) {
+                        })
+                        .then(function (response) {
                             Axios.post('/api/addCandy', {
                                 user: cookies.user.data[0].id,
                                 item: "Miel Ordinaire",
                                 slug: "honey",
                                 quantity: 1
-                            }).then(function (response) {
+                            })
+                            .then(function (response) {
                                 Axios.post('/api/addCandy', {
                                     user: cookies.user.data[0].id,
                                     item: "Bonbon S",
                                     slug: "exps",
                                     quantity: 10
-                                }).then(function (response) {
+                                })
+                                .then(function (response) {
                                     Axios.post('/api/addCandy', {
                                         user: cookies.user.data[0].id,
                                         item: "Bonbon S",
                                         slug: "exps",
                                         quantity: 10
-                                    }).then(function (response) {
+                                    })
+                                    .then(function (response) {
                                         Axios.post('/api/addCandy', {
                                             user: cookies.user.data[0].id,
                                             item: "Poke Ball",
                                             slug: "ball",
                                             quantity: 10
                                         })
-                                    }).then(function (response) {
-                                        Axios
-                                            .get("/api/getShinydex")
-                                            .then(function (response) {
-                                                setShinydex(response.data.sort((a, b) => b.id - a.id))
-                                            })
+                                        .then(function (response) {
+                                            Axios
+                                                .get("/api/getShinydex")
+                                                .then(function (response) {
+                                                    setShinydex(response.data.sort((a, b) => b.id - a.id))
+                                                })
+                                        })
                                     })
                                 })
                             })
                         })
                     }
-                }
-        }
+                })
+            }
     }, []);
     return (
         <div className={"globalContainerCenter"}>
