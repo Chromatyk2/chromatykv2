6e0ac2bc6b081b869b46294a80585c2fc1213c4c
pierre schuvey <chromatykretro@gmail.com>
2026-05-21 11:28:31 +0200

    flee fight

--- src/vues/leaderboard.js
+++ src/vues/leaderboard.js
@@ -3,15 +3,22 @@ import { Link } from "react-router-dom";
 import Axios from "axios";
 import moment from "moment/moment";
 import { useCookies } from 'react-cookie';
+import { getColorSync } from 'colorthief';
 
 
 function Leaderboard() {
     const [cookies, setCookie] = useCookies();
     const [leaderboard, setLeaderboard] = useState(null);
+    const [color, setColor] = useState(1);
     useEffect(() => {
         Axios.get("/api/getLeaderBoard/")
             .then((response) => {
                 setLeaderboard(response.data)
+                const img = new Image();
+                img.src = "/Skins/Trainer" + response.data[0].skin + ".png";
+                img.onload = () => {
+                    setColor(getColorSync(img).hex());
+                };
             })
     }, []);
     return (
@@ -26,7 +33,7 @@ function Leaderboard() {
                             <p className={"rank"}>{key+1}</p>
                             <div style={{width: "92%",display: "flex",justifyContent: "space-between"} }>
                                 <div className={"profilHeader"}>
-                                    <div style={{ width: "40px", height: "40px", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
+                                    <div style={{ width: "40px", height: "40px", backgroundColor: color, backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
                                     </div>
                                     {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                                     <div className={"profilInfos"}>
@@ -40,7 +47,7 @@ function Leaderboard() {
                                             <p style={{ fontSize: "14px", textAlign: "end" }}>{val.pokemon}</p>
                                             <p style={{ fontSize: "12px", textAlign: "end" }} className={"levelProfil"}>{val.shiny === 1 ? "Shiny" : val.negative === 1 ? "Négatif" : "Classique"}</p>
                                         </div>
-                                        <div style={{ width: "40px", height: "40px", filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: "rgba(0,0,0,0.3)", backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
+                                        <div style={{ width: "40px", height: "40px", filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: color, backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
                                         </div>
                                         {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                                     </div>
