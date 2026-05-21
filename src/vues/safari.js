dacbb68df2a7a3f8c038a0f8a343b6b78360481e
pierre schuvey <chromatykretro@gmail.com>
2026-05-21 11:11:49 +0200

    flee fight

--- src/vues/safari.js
+++ src/vues/safari.js
@@ -139,7 +139,7 @@ function Fight() {
                                             }                                         
                                             Axios.post('/api/updateXp', {
                                                 user: cookies.user.data[0].id,
-                                                xp: (pokemon.tier * 10) + bonusXP
+                                                xp: (Math.floor(Math.random() * (pokemon.tier * 10 + 1)) + pokemon.tier*10) + bonusXP
                                             })
                                             .then(function (response) {
                                                 setPokemon(null);
