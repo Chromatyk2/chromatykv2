ffe3547712469943d8d30cdb1962f99a69e2540f
pierre schuvey <chromatykretro@gmail.com>
2026-05-22 13:12:14 +0200

    flee fight

--- src/vues/leaderboard.js
+++ src/vues/leaderboard.js
@@ -68,7 +68,7 @@ function Leaderboard() {
                             <p className={"rank"}>{key+1}</p>
                             <div style={{width: "92%",display: "flex",justifyContent: "space-between"} }>
                                 <div className={"profilHeader"}>
-                                    <div style={{ width: "40px", height: "40px", backgroundColor: color, backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
+                                        <div style={{ width: "40px", height: "40px", backgroundColor: val.color, backgroundImage: `url("/Skins/Trainer${val.skin}.png")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"profilPicture"}>
                                     </div>
                                     {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                                     <div className={"profilInfos"}>
@@ -82,7 +82,7 @@ function Leaderboard() {
                                             <p style={{ fontSize: "14px", textAlign: "end" }}>{val.pokemon}</p>
                                             <p style={{ fontSize: "12px", textAlign: "end" }} className={"levelProfil"}>{val.shiny === 1 ? "Shiny" : val.negative === 1 ? "Négatif" : "Classique"}</p>
                                         </div>
-                                        <div style={{ width: "40px", height: "40px", filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: color, backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
+                                            <div style={{ width: "40px", height: "40px", filter: val.negative === 1 ? "invert(1)" : "invert(0)", backgroundColor: val.color, backgroundImage: `url("/Sprites/${val.shiny === 1 ? "Shiny" : "Normal"}/${val.number}.gif")`, backgroundRepeat: "no-repeat", backgroundSize: "contain", backgroundPosition: "center" }} className={"compagnonPicture"}>
                                         </div>
                                         {/*<img className={"profilPicture"} style={{ background: color }} src={"/Skins/Trainer"+profil[0].skin+".png"} />*/}
                                     </div>
