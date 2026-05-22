7f456ab2dc54e6e0fd931365a1e8713d4033db3f
pierre schuvey <chromatykretro@gmail.com>
2026-05-22 12:35:29 +0200

    flee fight

--- src/vues/compagnon.js
+++ src/vues/compagnon.js
@@ -251,7 +251,7 @@ function Compagnon() {
                                     <a className={"twitchLink"} href="https://twitch.tv/chromatyk" target="blank_">Twitch</a>
                                 </div>
                             )}
-                            {rareCandy.quantity > 0 &&
+                            {rareCandy?.quantity > 0 &&
                                 (
                                     <div onClick={() => levelupCompagnon(inventory.find(item => item.slug === "rarecandy").slug)} style={{ background: "none" }} className="fightActionsContainer">
                                         <div className="fightActions">
@@ -264,7 +264,7 @@ function Compagnon() {
                                     </div>
                                 )
                             }
-                            {megaCandy.quantity > 0 &&
+                            {megaCandy?.quantity > 0 &&
                                 (
                                 <div onClick={() => levelMaxCompagnon(inventory.find(item => item.slug === "megacandy").slug)} style={{ filter: "hue-rotate(182deg)", background: "none" }} className="fightActionsContainer">
                                         <div className="fightActions">
