3ea8269e2022441dcba13cb03b29e368c0d3e41a
pierre schuvey <chromatykretro@gmail.com>
2026-05-23 14:26:02 +0200

    fix

--- src/vues/profil.js
+++ src/vues/profil.js
@@ -44,7 +44,7 @@ function Profil() {
             const startDate = new Date(expedition.date);
             const endDate = new Date(expedition.endDate);
 
-            const now = new Date(now.getTime() + 2 * 60 * 60 * 1000);
+            const now = new Date(Date.now() + 2 * 60 * 60 * 1000)
 
             const totalDuration = endDate - startDate;
             const elapsed = now - startDate;
