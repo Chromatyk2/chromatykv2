e81ee24f671c5a0edba1999faa9a6299b578bec6
pierre schuvey <chromatykretro@gmail.com>
2026-06-01 01:56:09 +0200

    fix

--- src/components/fight.js
+++ src/components/fight.js
@@ -6,11 +6,23 @@ function ProgressBarFight(props) {
         <div className={"globalContainerCenter"}>
             <p>Compagnon</p>
             <div style={{ backgroundImage: `url(/gym.png)`, overflow: "overlay" }} className={"fightContainer"}>
-                <p className="fightName">{props.compagnon[0].pokemon}</p>
-                <div className="tierFight">
-                    Nv.{props.compagnon[0].level}
+                <div>
+                    <p className="fightName">{props.compagnon[0].pokemon}</p>
+                    <div className="tierFight">
+                        Nv.{props.compagnon[0].level}
+                    </div>
+                    <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
                 </div>
-                <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
+                <div>
+                    <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
+                </div>
+                <div>
+                    <p className="fightName">{props.compagnon[0].pokemon}</p>
+                    <div className="tierFight">
+                        Nv.{props.compagnon[0].level}
+                    </div>
+                    <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
+                </div>
             </div>
         </div>
     )
