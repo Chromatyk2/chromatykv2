17d8508f99560b9bea3a9be23fa712d091eefe14
pierre schuvey <chromatykretro@gmail.com>
2026-06-01 07:07:54 +0200

    fix

--- src/components/fight.js
+++ src/components/fight.js
@@ -239,7 +239,7 @@ function ProgressBarFight(props) {
                     <div className={"progressBarFightExternalXp"}>
                         <div style={{ width: (currentXp / xpToNextLevel) * 100+"%" }} className={"progressBarFightInternalXp"}>
                         </div>
-                        <p>{currentXp + " / " + xpToNextLevel}</p>
+                        <p>{(currentXp / xpToNextLevel) * 100 + " %"}</p>
                     </div>
                     </div>
                 </>
