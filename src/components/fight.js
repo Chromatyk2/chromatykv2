5bfe70d8d7c3815d826ba656a8509515259134c2
pierre schuvey <chromatykretro@gmail.com>
2026-06-01 02:14:23 +0200

    fix

--- src/components/fight.js
+++ src/components/fight.js
@@ -3,25 +3,25 @@ import '../App.css'
 
 function ProgressBarFight(props) {
     return (            
-        <div className={"globalContainerCenter"}>
+        <div style={{flexDirection:"row", flexWrap:"wrap"}} className={"globalContainerCenter"}>
             <p>Compagnon</p>
             <div style={{ backgroundImage: `url(/gym.png)`, overflow: "overlay" }} className={"fightContainer"}>
-                <div>
+                <div style={{width:"30%"}}>
                     <p className="fightName">{props.compagnon[0].pokemon}</p>
                     <div className="tierFight">
                         Nv.{props.compagnon[0].level}
                     </div>
-                    <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
+                    <div className="fightSpriteCard" style={{height:"200px", width:"100%", filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
                 </div>
-                <div>
-                    <div className="fightSpriteCard" style={{backgroundSize: "contain", backgroundImage: `url(/versus.png)` }} />
+                <div style={{ width: "33%" }}>
+                    <div className="fightSpriteCard" style={{ width: "100%", backgroundSize: "contain", backgroundImage: `url(/versus.png)` }} />
                 </div>
-                <div>
+                <div style={{ width: "30%" }}>
                     <p className="fightName">{props.compagnon[0].pokemon}</p>
                     <div className="tierFight">
                         Nv.{props.compagnon[0].level}
                     </div>
-                    <div className="fightSpriteCard" style={{ filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
+                    <div className="fightSpriteCard" style={{ height: "200px", width: "100%", filter: props.compagnon[0].negative === 1 ? "invert(1)" : "none", backgroundSize: "contain", backgroundImage: `url(/Sprites/${props.compagnon[0].shiny === 1 ? "shiny" : "normal"}/${props.compagnon[0].number}.gif)` }} />
                 </div>
             </div>
         </div>
