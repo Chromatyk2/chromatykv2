15883c7807e4931fe7f39ac1e9702c88379059b6
pierre schuvey <chromatykretro@gmail.com>
2026-06-02 00:53:05 +0200

    fix

--- src/components/fight.js
+++ src/components/fight.js
@@ -244,22 +244,22 @@ function ProgressBarFight(props) {
                         <div style={{ position:"relative", width: "30%" }}>
                         <p className="fightName">{pokemon.name}</p>
                         <div style={{display:"block",margin:"auto", backgroundColor: pokemon.tier == 1 ? "#6d6d6c" : pokemon.tier == 2 ? "#21693a" : pokemon.tier == 3 ? "#744095" : "#bfa93a" }} className={"tierFight"}>Tier {pokemon.tier}</div>
-                        {damageText && (
-                            <div
-                                className={`damageText ${damageText.critical ? "critical" : ""}`}
-                                style={{
-                                    left: `${damageText.offsetLeft * 2}px`,
-                                    top: `${damageText.offsetTop * 2}px`,
-                                    "--offsetX": `${damageText.driftX}px`,
-                                    "--rotation": `${damageText.rotation}deg`,
-                                    "--arcX": `${Math.random() * 120 - 60}px`
-                                }}
-                            >
-                                -{damageText.value}
-                            </div>
-                        )}
+                        
                         <div className={`fightSpriteCardEnemy ${isKO ? "koAnimation" : ""} ${!hasAppeared ? "spawn" : ""} ${isAttacking ? "hit" : ""}`} style={{ height: "200px", width: "100%", filter: negative === 1 && "invert(1)", backgroundSize: "contain", backgroundImage: `url(/Sprites/${shiny === 1 ? "shiny" : "normal"}/${pokemon.number}.gif)` }}>
-                            
+                            {damageText && (
+                                <div
+                                    className={`damageText ${damageText.critical ? "critical" : ""}`}
+                                    style={{
+                                        left: `${damageText.offsetLeft * 2}px`,
+                                        top: `${damageText.offsetTop * 2}px`,
+                                        "--offsetX": `${damageText.driftX}px`,
+                                        "--rotation": `${damageText.rotation}deg`,
+                                        "--arcX": `${Math.random() * 120 - 60}px`
+                                    }}
+                                >
+                                    -{damageText.value}
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className="hpBarContainer">
