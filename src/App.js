a1fb2d92d883889fab5a950b23d716b054980099
pierre schuvey <chromatykretro@gmail.com>
2026-06-05 01:03:56 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Axios from "axios";
 import Log from "./services/log";
 
 //Publique
-import TwitchLiveWidget from "./composants/twitchLiveWidget";
+import TwitchLiveWidget from "./components/twitchLiveWidget";
 import NavBar from './vues/navbar.js';
 import HomePage from './vues/home.js';
 import ShinyDex from './vues/Shinydex.js';
