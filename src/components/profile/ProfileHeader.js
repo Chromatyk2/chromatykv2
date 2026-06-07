469eaff48319ff51c0004e0ac69a1626a9e2011b
pierre schuvey <chromatykretro@gmail.com>
2026-06-07 23:26:00 +0200

    fix

--- src/components/profile/ProfileHeader.js
+++ src/components/profile/ProfileHeader.js
@@ -3,6 +3,80 @@ function ProfileHeader({
     companion,
     color
 }) {
+    const animatedSkins = new Set([
+        // 1337 -> 1452
+        ...Array.from(
+            { length: 1452 - 1337 + 1 },
+            (_, i) => 1337 + i
+        ),
+
+        1635,
+        1636,
+        1637,
+
+        1639,
+
+        1642,
+        1643,
+        1644,
+
+        1646,
+        1647,
+        1648,
+        1649,
+        1650,
+        1651,
+
+        1653,
+        1654,
+        1655,
+        1656,
+        1657,
+        1658,
+        1659,
+
+        1662,
+        1663,
+
+        1672,
+
+        1679,
+        1680,
+
+        1683,
+
+        1687,
+
+        1690,
+        1691,
+
+        1694,
+        1695,
+
+        1698,
+
+        1702,
+
+        1704,
+        1705,
+
+        1707,
+
+        1709,
+
+        1711,
+        1712,
+
+        // 2040 -> 2070
+        ...Array.from(
+            { length: 2070 - 2040 + 1 },
+            (_, i) => 2040 + i
+        )
+    ]);
+    const isAnimated =
+        animatedSkins.has(
+            profile.skin
+        );
     const currentLevelXp =
         25 *
         profile.level *
@@ -30,8 +104,7 @@ function ProfileHeader({
 
     <div className="trainerContainer">
 
-        <img
-                        src={`/SkinsCentered/Trainer${profile.skin}.png`}
+            <img style={{top: isAnimated ? "-10px" : "-29px"}} src={`/SkinsCentered/Trainer${profile.skin}.png`}
             alt={profile.login}
             className="trainerSprite"
         />
