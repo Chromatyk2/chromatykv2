f2cbd507b0eddcada6739d3f9a0dcb8e60e33e4f
pierre schuvey <chromatykretro@gmail.com>
2026-06-09 08:15:53 +0200

    FIX ACHAT LIVE3

--- src/pages/inventory.js
+++ src/pages/inventory.js
@@ -72,11 +72,11 @@ function Inventory(props) {
                 };
             case "boxplus":
                 return {
-                    filter: "drop-shadow(red 0px 0px 5px) contrast(1.7)";
+                    filter: "drop-shadow(red 0px 0px 5px) contrast(1.7)"
                 };
             case "fragement":
                 return {
-                    filter: "drop-shadow(red 0px 0px 5px) contrast(1.7)";
+                    filter: "drop-shadow(red 0px 0px 5px) contrast(1.7)"
                 };
             default:
                 return {};
