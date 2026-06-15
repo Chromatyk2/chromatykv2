b1a4629b136e597486dab034bd6cb8c3873904e8
pierre schuvey <chromatykretro@gmail.com>
2026-06-15 21:57:08 +0200

    fix sprite size

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,17 @@
 import "./Loader.css";
+import React, { useState, useEffect } from 'react';
 
 function Loader() {
+    const [pokemonId] = useState(
+        () => Math.floor(Math.random() * 1398) + 1
+    );
+
 
     return (
         <div className="loaderContainer">
 
             <img
-                src={"/Sprites/shiny/" + (Math.floor(Math.random() * 1398) + 1) +".gif"}
+                src={`/Sprites/shiny/${pokemonId}.gif`}
                 alt="Loading"
                 className="loaderPokemon"
             />
