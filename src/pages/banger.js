a44f3502c0c6412a08cfd58b1ffa491d20cbfddf
pierre schuvey <chromatykretro@gmail.com>
2026-06-26 14:46:59 +0200

    Get Banger

--- src/pages/banger.js
+++ src/pages/banger.js
@@ -65,7 +65,7 @@ function Banger(props) {
                 <div
                     className="skinRevealImage"
                     style={{
-                        backgroundImage: `url("/jaquettes/${banger.console}/${banger.number}.png")`
+                        backgroundImage: `url("/jaquettes/${banger.console}/Jeu (${banger.number}).png")`
                     }}
                 />
             )}
